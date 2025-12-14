faee57815f9c4f6a1176857d3beb6f71d8e2426d
angie ♥ <angeles.veron03@gmail.com>
2025-12-14 18:49:24 -0300

    arreglando config de tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind"; // ⬅️ 1. Importación de la integración correcta
 
+import tailwind from "@astrojs/tailwind"; 
 
 import react from '@astrojs/react';
 
-
 // https://astro.build/config
 export default defineConfig({
-    // 2. Añadimos la integración a la lista de integraciones
     integrations: [tailwind(), react()], 
     
-    // 3. Puedes eliminar cualquier sección 'vite: { plugins: [...] }'
-    //    que contenga la configuración antigua de Tailwind.
+   
 });
